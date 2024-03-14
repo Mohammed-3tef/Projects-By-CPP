d4aed20f21316731263c14d418872b56c00dcf9d
Esraa Emary <162044733+esraa-emary@users.noreply.github.com>
2024-03-14 22:44:08 +0200

    Add files via upload
    Base for task 3 in assignment 2

---
+++ CS112_A2_T3_S27_20230054_20231109_20231143.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//check the validity of the entered numbers
+void validity(){
+
+
+}
+
+//function for Rational Number Calculator
+string R_N_C(){
+
+
+
+}
+
+//main application
+int main(){
+    while (true)
+    {
+        cout << "#=====Welcome to our application=====#\nWhat do you want to do?\n[1]Rational Number Calculator.\n[2]Exit.\nChoice: ";
+        int choice;
+        cin>> choice;
+        while ((cin.fail())||(choice!= 1 && choice!= 2)){
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            cout<< "Invalid input."<<endl;
+            cout<< "Please enter a valid choice: ";
+            cin>>choice;
+        }  
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        
+        if(choice == 1){
+            
+
+
+
+
+
+
+            int choice2;
+            cout << "Do you want to continue?\n[1]Yes.\n[2]No.\nChoice: ";
+            cin >> choice2;
+            while ((cin.fail())||(choice!= 1 && choice!= 2)){
+                cin.clear();
+                cin.ignore(numeric_limits<streamsize>::max(), '\n');
+                cout<< "Invalid input."<<endl;
+                cout<< "Please enter a valid choice: ";
+                cin>>choice;
+            }  
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            if(choice2 == 1)
+                continue;
+            else if(choice2 == 2)
+                break;
+        }
+        if(choice == 2)
+            break;
+    }
+    cout << "Thanks for using our application..."<<endl;
+    return 0;
+}
